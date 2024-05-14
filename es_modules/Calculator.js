2a83bdb444cb1d405d4c3f3fb19fe111411882b8
Ekrash Zahid <f2022065090@umt.edu.pk>
2024-05-14 01:31:59 -0700

    arithemetic operation done 2 left behind

--- es_modules/Calculator.js
+++ es_modules/Calculator.js
@@ -3,7 +3,7 @@ import chalk from "chalk";
 console.log("Hello! I am Ekrash Calculator");
 const MainMenu = await inquirer.prompt([
     {
-        type: 'list',
+        type: "list",
         name: "mainMenuChoices",
         message: chalk.bgMagenta.blue("Please Choose Category"),
         choices: [
@@ -11,5 +11,58 @@ const MainMenu = await inquirer.prompt([
             chalk.bold.blue("Scientific"),
             chalk.bold.red("Exit The Calculator"),
         ],
-    }
+    },
 ]);
+const Arithmeticmenu = await inquirer.prompt([
+    {
+        type: "list",
+        name: "arthmeticop",
+        message: chalk.bgMagenta.blue("Please Choose the Arithmetic Operator"),
+        choices: [
+            chalk.bold.green("Subtraction -"),
+            chalk.bold.blue("Addition +"),
+            chalk.bold.gray("Multiplication *"),
+            chalk.bold.white("Division /"),
+            chalk.bold.red("Back to Menu"),
+        ],
+    },
+]);
+//console.log(MainMenu);
+//console.log(Arithmeticmenu);
+if (Arithmeticmenu.arthmeticop != chalk.bold.red("Back to  Menu")) {
+    const number = await inquirer.prompt([
+        {
+            type: "input",
+            name: "Number1",
+            Message: chalk.blueBright("Enter the Number 1"),
+        },
+        {
+            type: "input",
+            name: "Number2",
+            Message: chalk.blueBright("Enter the Number 2"),
+        },
+    ]);
+    let result;
+    if (Arithmeticmenu.arthmeticop == chalk.bold.green("Subtraction -")) {
+        result = number.Number1 - number.Number2;
+        console.log(chalk.greenBright(`The Answer For your Required Operation is as ${result}`));
+    }
+    else if (Arithmeticmenu.arthmeticop == chalk.bold.blue("Addition +")) {
+        result = number.Number1 + number.Number2;
+        console.log(chalk.greenBright(`The Answer For your Required Operation is as ${result}`));
+    }
+    else if (Arithmeticmenu.arthmeticop == chalk.bold.gray("Multiplication *")) {
+        result = number.Number1 * number.Number2;
+        console.log(chalk.greenBright(`The Answer For your Required Operation is as ${result}`));
+    }
+    else if (Arithmeticmenu.arthmetiop == chalk.bold.white("Division /")) {
+        if (number.numer1 == 0) {
+            console.log(chalk.bgRedBright('NO POSSIBLE'));
+        }
+        result = number.Number1 / number.Number2;
+        console.log(chalk.greenBright(`The Answer For your Required Operation is as ${result}`));
+    }
+    else {
+        console.log("Invalid Input");
+    }
+}
--- es_modules/Calculator.ts
+++ es_modules/Calculator.ts
@@ -1,22 +1,80 @@
 import inquirer from "inquirer";
 import chalk from "chalk";
 import Choice from "inquirer/lib/objects/choice.js";
+import prompt from "inquirer/lib/ui/prompt.js";
 console.log("Hello! I am Ekrash Calculator");
 
-
-
 const MainMenu = await inquirer.prompt([
-    {
-      type: 'list',
-      name: "mainMenuChoices",
-      message: chalk.bgMagenta.blue("Please Choose Category"), 
-      choices: [
-        chalk.bold.green("Arithmetic Operation"), 
-        chalk.bold.blue("Scientific"),
-        chalk.bold.red("Exit The Calculator"), 
-      ],
-    }
+  {
+    type: "list",
+    name: "mainMenuChoices",
+    message: chalk.bgMagenta.blue("Please Choose Category"),
+    choices: [
+      chalk.bold.green("Arithmetic Operation"),
+      chalk.bold.blue("Scientific"),
+      chalk.bold.red("Exit The Calculator"),
+    ],
+  },
 ]);
+
+const Arithmeticmenu = await inquirer.prompt([
+  {
+    type: "list",
+    name: "arthmeticop",
+    message: chalk.bgMagenta.blue("Please Choose the Arithmetic Operator"),
+    choices: [
+      chalk.bold.green("Subtraction -"),
+      chalk.bold.blue("Addition +"),
+      chalk.bold.gray("Multiplication *"),
+      chalk.bold.white("Division /"),
+      chalk.bold.red("Back to Menu"),
+    ],
+  },
+]);
+
+//console.log(MainMenu);
+//console.log(Arithmeticmenu);
+if (Arithmeticmenu.arthmeticop != chalk.bold.red("Back to  Menu")) {
+  const number = await inquirer.prompt([
+    {
+      type: "input",
+      name: "Number1",
+      Message: chalk.blueBright("Enter the Number 1"),
+    },
+    {
+      type: "input",
+      name: "Number2",
+      Message: chalk.blueBright("Enter the Number 2"),
+    },
+  ]);
+
+  let result;
+  if (Arithmeticmenu.arthmeticop == chalk.bold.green("Subtraction -")) {
+    result = number.Number1 - number.Number2;
+    console.log(chalk.green(`The Answer For your Required Operation is as ${result}`));
+  } else if (Arithmeticmenu.arthmeticop == chalk.bold.blue("Addition +")) {
+    result = number.Number1 + number.Number2;
+    console.log(chalk.green(`The Answer For your Required Operation is as ${result}`));
+  } else if (
+    Arithmeticmenu.arthmeticop == chalk.bold.gray("Multiplication *")
+  ) {
+    result = number.Number1 * number.Number2;
+    console.log(chalk.green(`The Answer For your Required Operation is as ${result}`));
+  } else if (Arithmeticmenu.arthmetiop == chalk.bold.white("Division /")) {
+    if(number.numer1==0)
+      {
+        console.log(chalk.bgRedBright('NO POSSIBLE'));
+      }
+    result=number.Number1/number.Number2;
+    console.log(chalk.green(`The Answer For your Required Operation is as ${result}`));
+
+  }
+
+    else 
+    {
+      console.log("Invalid Input");
+    }
+}
 
 
 
